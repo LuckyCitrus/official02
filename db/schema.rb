022b7864e1d1fe5dfa44cbc4b08745a0451c37ec
unknown <kietnguyen2804@gmail.com>
2020-03-16 00:53:27 -0500

    pre-auction

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_054419) do
+ActiveRecord::Schema.define(version: 2020_03_16_055246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,12 @@ ActiveRecord::Schema.define(version: 2020_03_16_054419) do
 
   create_table "hellos", force: :cascade do |t|
     t.string "world"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "city"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
