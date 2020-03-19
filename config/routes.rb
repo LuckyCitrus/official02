7241f9986f81a733ffd5f2da21f90cd08f9fe26c
unknown <kietnguyen2804@gmail.com>
2020-03-19 00:00:32 -0500

    pre-containerOrders

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :containers
   resources :dummyusers
   resources :dummyroles
   resources :invoices
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_044127) do
+ActiveRecord::Schema.define(version: 2020_03_19_045912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 2020_03_19_044127) do
     t.string "companyname"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "containers", force: :cascade do |t|
+    t.bigint "company_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["company_id"], name: "index_containers_on_company_id"
   end
 
   create_table "countries", force: :cascade do |t|
@@ -277,6 +284,7 @@ ActiveRecord::Schema.define(version: 2020_03_19_044127) do
   add_foreign_key "auctions", "locations"
   add_foreign_key "cars", "keystatuses"
   add_foreign_key "cars", "titlestatuses"
+  add_foreign_key "containers", "companies"
   add_foreign_key "customers", "countries"
   add_foreign_key "customers", "customerstatuses"
   add_foreign_key "customers", "customertypes"
