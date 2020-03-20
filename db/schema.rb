5a91618023edf06ca3d55fc874a814c2c3a2de97
unknown <kietnguyen2804@gmail.com>
2020-03-19 22:50:15 -0500

    pre-containerId

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_053658) do
+ActiveRecord::Schema.define(version: 2020_03_19_055527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -252,7 +252,6 @@ ActiveRecord::Schema.define(version: 2020_03_19_053658) do
   create_table "shipments", force: :cascade do |t|
     t.date "date"
     t.integer "quantity"
-    t.string "container"
     t.bigint "warehouse_id", null: false
     t.bigint "shipmentmethod_id", null: false
     t.bigint "shipmentstatus_id", null: false
