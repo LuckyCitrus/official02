5eb8fd7ff8477810417e0a628cd870a7ffefa0a9
unknown <kietnguyen2804@gmail.com>
2020-03-23 00:08:50 -0500

    pre-modelAdjust

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,39 @@ Location.create!([
   {city: "Dallas"},
   {city: "Conroe"}
 ])
+Department.create!([
+  {departmentname: "Logistics", company_id: 1},
+  {departmentname: "Human Resources", company_id: 2}
+])
+Employeestatus.create!([
+  {employeestatus: "Active"},
+  {employeestatus: "Inactive"},
+  {employeestatus: "Vacation"},
+  {employeestatus: "Sick Leave"}
+])
+Dummyrole.create!([
+  {role: "Admin"},
+  {role: "Employee"},
+  {role: "Customer"}
+])
+Country.create!([
+  {country_name: "United States"},
+  {country_name: "Dubai"},
+  {country_name: "Jordan"}
+])
+Customertype.create!([
+  {customertype: "Returned"},
+  {customertype: "New"},
+  {customertype: "Returning"}
+])
+Customerstatus.create!([
+  {customerstatus: "In-line"},
+  {customerstatus: "Appointed"},
+  {customerstatus: "Occurred "}
+])
+Auction.create!([
+  {auctionname: "Copart", address: "1655 RANKIN ROAD HOUSTON, TEXAS 77073 4903", location_id: 1},
+  {auctionname: "INSURANCE AUTO AUCTIONS INC", address: "2535 W Mount, Houston, TX 77038", location_id: 1},
+  {auctionname: "Auction 2", address: "222 Two", location_id: 2},
+  {auctionname: "Auction 3", address: "333 Three", location_id: 3}
+])
