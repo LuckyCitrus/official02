3abe51037949a85cf34c48c2083776e727ff33d2
Kiet Nguyen <kietnguyen2804@gmail.com>
2020-03-24 01:10:06 -0500

    pre-employee

--- db/seeds.rb
+++ db/seeds.rb
@@ -101,3 +101,8 @@ Paymentmethod.create!([
   {paymenttype: "Bank Wire"}
 ])
 
+Customer.create!([
+  {first_name: "CustFName01", last_name: "CustLName01", address: "CustAddress01", phone: "(111)111-1111", email: "Cust01@email.com", country_id: nil, customerstatus_id: nil, customertype_id: nil, dummyuser_id: nil},
+  {first_name: "CustFName02", last_name: "CustLName02", address: "CustAddress02", phone: "(222)222-2222", email: "Cust02@email.com", country_id: nil, customerstatus_id: nil, customertype_id: nil, dummyuser_id: nil},
+  {first_name: "CustFName03", last_name: "CustLName03", address: "CustAddress03", phone: "(333)333-3333", email: "Cust03@email.com", country_id: nil, customerstatus_id: nil, customertype_id: nil, dummyuser_id: nil}
+])
