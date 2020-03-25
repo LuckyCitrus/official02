a0e0958e9c8f2196e477f6af9747c1c86ca35f27
Kiet Nguyen <kietnguyen2804@gmail.com>
2020-03-25 01:29:13 -0500

    pre-paymentNum

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -70,6 +70,6 @@ class OrdersController < ApplicationController
     # Only allow a list of trusted parameters through.
     def order_params
       # params.require(:order).permit(:date, :lotstock, :quantity, :price, :total, :orderstatus_id, :picture_id)
-      params.require(:order).permit(:ordernum, :date, :lotstock, :quantity, :price, :total, :orderstatus_id)
+      params.require(:order).permit(:ordernum, :date, :lotstock, :quantity, :price, :total, :orderstatus_id, :customer_id)
     end
 end
