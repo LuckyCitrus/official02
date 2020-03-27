31dce0671dd31ca8e2f174b1bb2132ff8c932d83
LuckyCitrus <kristyhoang@gmail.com>
2020-03-26 23:41:19 -0500

    pre-relationships

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,7 +2,7 @@ class Employee < ApplicationRecord
   # belongs_to :company
   belongs_to :department
   belongs_to :employeestatus
-  belongs_to :dummyuser
+  #belongs_to :dummyuser
 
   def emp_fullname
     "#{empfname} #{emplname}"
