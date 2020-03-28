538cccb857fce1efc53db1d3bff47f6386bc13a1
Kiet Nguyen <kietnguyen2804@gmail.com>
2020-03-28 01:34:23 -0500

    post-companyDepartmentsNested

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,4 @@
 class Company < ApplicationRecord
-  has_many :departments
+  has_many :departments, dependent: :destroy
+  accepts_nested_attributes_for :departments, reject_if: :all_blank, allow_destroy: :true
 end
