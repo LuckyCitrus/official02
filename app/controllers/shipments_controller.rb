b75759183407fafd799d95c7f88fa41470cbbec8
LuckyCitrus <kristyhoang@gmail.com>
2020-03-28 01:09:14 -0500

    pre-shipmentpolicy

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,5 +1,5 @@
 class ShipmentsController < ApplicationController
-  #before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :set_shipment, only: [:show, :edit, :update, :destroy]
   #load_and_authorize_resource
 
