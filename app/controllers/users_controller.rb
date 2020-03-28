f6c8a77f4ae1192dc36adea17114f84c2509277d
LuckyCitrus <kristyhoang@gmail.com>
2020-03-27 21:54:18 -0500

    post-userpolicy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  #before_action :authenticate_user!, :admin_only
+  before_action :authenticate_user!, :admin_only
   #load_and_authorize_resource
 
   def index
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     unless current_user.admin?
       unless @user == current_user
-        redirect_to root_path, :alert => "Access denied."
+        redirect_to root_path, :alert => "Access denied. Insufficient permissions."
       end
     end
   end
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(secure_params)
-      redirect_to users_path, :notice => "User updated."
+      redirect_to users_path, :notice => "User was successfully updated."
     else
       redirect_to users_path, :alert => "Unable to update user."
     end
@@ -27,14 +27,14 @@ class UsersController < ApplicationController
   def destroy
     user = User.find(params[:id])
     user.destroy
-    redirect_to users_path, :notice => "User deleted."
+    redirect_to users_path, :notice => "User was successfully deleted."
   end
 
   private
 
   def admin_only
     unless current_user.admin?
-      redirect_to root_path, :alert => "Access denied."
+      redirect_to root_path, :alert => "Access denied. Insufficient permissions."
     end
   end
 
