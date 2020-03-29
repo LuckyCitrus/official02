17f11a68c187c648472eb84d9a1dafd4b3fd49c4
LuckyCitrus <kristyhoang@gmail.com>
2020-03-29 00:43:51 -0500

    post-paymentvalidation

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,4 +3,13 @@ class Payment < ApplicationRecord
   belongs_to :paymentmethod
   belongs_to :customer
   belongs_to :invoice
+
+  validates :paymentnum, format: { with: /\A[a-z\d][a-z\d-]*[a-z\d-]\z/i }, presence: true
+  validates :date, presence: true
+  validates :customer_id, presence: true
+  validates :amount, numericality: { greater_than_or_equal_to: 0}, presence: true
+  validates :paymentstatus_id, presence: true
+  validates :paymentmethod_id, presence: true
+  validates :invoice_id, presence: true
+
 end
