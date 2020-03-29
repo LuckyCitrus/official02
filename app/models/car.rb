bff4978cced27f4693115227fdf5ed83c9ce0061
LuckyCitrus <kristyhoang@gmail.com>
2020-03-28 23:41:14 -0500

    post-containervalidation

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,10 +3,10 @@ class Car < ApplicationRecord
    belongs_to :keystatus
    belongs_to :order
 
-  validates :vinnumber, presence: true, length: { is: 17}, format: { with: /\A[a-z\d][a-z\d-]*[a-z\d]\z/i }
+  validates :vinnumber, presence: true, length: { minimum: 11, maximum: 17}, format: { with: /\A[a-z\d][a-z\d-]*[a-z\d]\z/i }
   validates :year, presence: true, inclusion: { in: 1900..Date.today.year+1 }, format: { with: /(19|20)\d{2}/i }
-  validates :make, presence: true
-  validates :model, presence: true
+  validates :make, format: { with: /\A[a-z\d][a-z\d-]*[a-z\d-]\z/i }, presence: true
+  validates :model, format: { with: /\A[a-z\d][a-z\d-]*[a-z\d-]\z/i }, presence: true
   validates :titlestatus_id, presence: true
   validates :keystatus_id, presence: true
   validates :order_id, presence: true
--- app/models/container.rb
+++ app/models/container.rb
@@ -6,4 +6,8 @@ class Container < ApplicationRecord
   has_many :containerorders
   has_many :orders, through: :containerorders
 
+  validates :containernum, format: { with: /\A[a-z\d][a-z\d-]*[a-z\d-]\z/i }, presence: true
+  validates :shipment_id, presence: true
+  validates :company_id, presence: true
+
 end
