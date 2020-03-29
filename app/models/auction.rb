007d275e5c7273e19dd441c4c4c13406901cb59a
LuckyCitrus <kristyhoang@gmail.com>
2020-03-29 18:35:27 -0500

    post-auctionvalidation

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -3,5 +3,10 @@ class Auction < ApplicationRecord
 
   #Order Round
   has_many :order_auctions
-  has_many :orders, through: :order_autions
+  has_many :orders, through: :order_auctions
+
+  validates :auctionname, presence: true
+  validates :address, presence: true
+  validates :location_id, presence: true
+
 end
