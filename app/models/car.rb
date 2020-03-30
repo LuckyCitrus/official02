37692df36f8a75540d7c49d9673e8d53fff7e245
LuckyCitrus <kristyhoang@gmail.com>
2020-03-29 19:14:52 -0500

    post-countryvalidation

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,7 +3,7 @@ class Car < ApplicationRecord
    belongs_to :keystatus
    belongs_to :order
 
-  validates :vinnumber, presence: true, length: { minimum: 11, maximum: 17}
+  validates :vinnumber, presence: true, length: { minimum: 11, maximum: 17 }
   validates :year, presence: true, inclusion: { in: 1900..Date.today.year+1 }, format: { with: /(19|20)\d{2}/i }
   validates :make, presence: true
   validates :model, presence: true
