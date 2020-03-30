aa00b2fc32c5cb75fc7b416a5893836e59151c0c
LuckyCitrus <kristyhoang@gmail.com>
2020-03-29 19:57:50 -0500

    post-locationvalidation

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,4 +3,6 @@ class Location < ApplicationRecord
   has_many :warehouses
   has_many :shipments, through: :warehouses
 
+  validates :city, presence: true, length: { minimum: 2, maximum: 36 }
+
 end
