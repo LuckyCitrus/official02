b6e0bf28669011339ea3a158b160916327408dc8
LuckyCitrus <kristyhoang@gmail.com>
2020-03-31 15:54:18 -0500

    customertypecard

--- db/migrate/20200326040715_devise_create_users.rb
+++ db/migrate/20200326040715_devise_create_users.rb
@@ -33,7 +33,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       # t.datetime :locked_at
 
 
-      t.timestamps null: false
+      t.timestamps null: true
     end
 
     add_index :users, :email,                unique: true
