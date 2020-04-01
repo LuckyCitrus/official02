9ac65a6f06fa24a796868656876e1e47dab70caa
LuckyCitrus <kristyhoang@gmail.com>
2020-04-01 03:13:08 -0500

    formatdate

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,7 +21,8 @@ require("flatpickr/dist/flatpickr.css")
 document.addEventListener("turbolinks:load", () => {
 	flatpickr("[data-behavior='flatpickr']", {
 		altInput: true,
-		altFormat: "F j, Y",
-		dateFormat: "Y-m-d"
+		altFormat: "l, F j, Y",
+		dateFormat: "m-d-y",
+		allowInput: true
 	})
 });
