1dae135eb761893a3d556d3e67dc8909cfc30e7d
Kiet's Desktop <kmnguy25@gmail.com>
2020-04-02 14:57:35 -0500

    post_countriesIncremental

--- db/migrate/20200402192115_edit_country_incremental.rb
+++ db/migrate/20200402192115_edit_country_incremental.rb
@@ -1,7 +1,7 @@
 class EditCountryIncremental < ActiveRecord::Migration[6.0]
   def change
     reversible do |dir|
-      dir.up { execute "ALTER TABLE countries AUTO_INCREMENT = 100" }
+      dir.up { execute "ALTER SEQUENCE countries_id_seq RESTART WITH 100" }
     end
   end
 end
