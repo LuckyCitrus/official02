5e893cb0692fa74ce761f62c5b441c8fbed97d02
LuckyCitrus <kristyhoang@gmail.com>
2020-04-07 01:08:17 -0500

    pdfbutton

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -30,7 +30,17 @@ class ReportsController < ApplicationController
 	end
 
 	def auction_orders
-    	@auction_orders = AuctionOrder.all
+		@auction_orders = AuctionOrder.all
+		respond_to do |format|
+			format.html
+			format.pdf do
+				pdf = AuctionOrderPdf.new(@auction_orders)
+				send_data pdf.render,
+				filename: "Auction_Total_Orders_" + Date.today.to_s(:num_date) + ".pdf",
+				type: "application/pdf",
+				disposition: "inline"
+			end
+		end
 	end
 	
 end
