5fbf5219645960199cee61dea55982505bb71476
Kiet's Desktop <kmnguy25@gmail.com>
2020-04-08 02:47:14 -0500

    post_imgPolicy

---
+++ app/policies/image_policy.rb
@@ -0,0 +1,19 @@
+class ImagePolicy < ApplicationPolicy
+  attr_reader :user, :image
+
+  def initialize(user, image)
+    @user = user
+    @image = image
+  end
+
+  class Scope < Scope
+    def resolve
+      if user.admin? || user.employee
+        scope.all
+      else
+        scope.includes(:customer).where(customers: { user_id: user.id })
+      end
+
+    end
+  end
+end
