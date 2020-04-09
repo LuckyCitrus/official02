9d62ab4c1b7123a9cd750dbebb9d692d6bef3d2a
LuckyCitrus <kristyhoang@gmail.com>
2020-04-08 21:11:09 -0500

    sendgridconfig

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,8 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { host: 'mitsabfaro.com' }
+  config.action_mailer.default_url_options = { host: 'mitsabfaro.com', protocol: 'https' }
+  #config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net'} 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 end
