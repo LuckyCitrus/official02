0ff8d3dcaea5e1842edf43e16c1870397afe4af7
Kiet's Desktop <kmnguy25@gmail.com>
2020-04-10 02:13:33 -0500

    post_www

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { host: 'mitsabfaro.com', protocol: 'http' }
+  config.action_mailer.default_url_options = { host: 'www.mitsabfaro.com', protocol: 'http' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 end
