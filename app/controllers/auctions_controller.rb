160018b15c5edf9b2750406eb4249dfd499e0c79
Kiet's Desktop <kmnguy25@gmail.com>
2020-04-12 11:18:01 -0500

    post_orderFixed

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -6,7 +6,7 @@ class AuctionsController < ApplicationController
   # GET /auctions.json
   def index
     #@auctions = Auction.order(:location_id)
-    @pagy, @auctions = pagy(Auction.order(:location_id))
+    @pagy, @auctions = pagy(Auction.order('auctions.created_at DESC'))
   end
 
   # GET /auctions/1
