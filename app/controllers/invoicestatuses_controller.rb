19b5cd5a0e77d5322bb52458f6f4f809715ca558
Kiet's Desktop <kmnguy25@gmail.com>
2020-04-12 16:22:44 -0500

    post_invoiceStatusFix'

--- app/controllers/invoicestatuses_controller.rb
+++ app/controllers/invoicestatuses_controller.rb
@@ -5,7 +5,7 @@ class InvoicestatusesController < ApplicationController
   # GET /invoicestatuses
   # GET /invoicestatuses.json
   def index
-    @invoicestatuses = Invoicestatus.all
+    @invoicestatuses = Invoicestatus.all.order(created_at: :desc)
   end
 
   # GET /invoicestatuses/1
