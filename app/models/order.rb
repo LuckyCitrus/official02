56051587e510d8401b89551a88d8d91532b8edf2
LuckyCitrus <kristyhoang@gmail.com>
2020-04-14 14:53:11 -0500

    orderview

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,21 +8,22 @@ class Order < ApplicationRecord
   belongs_to :auction
 
   #delete cascading
-  has_many :cars, dependent: :destroy
+  has_one :car, dependent: :destroy
   has_many :images, dependent: :destroy
 
   #nested forms
-  accepts_nested_attributes_for :cars, reject_if: :all_blank, allow_destroy: :true
+  #accepts_nested_attributes_for :cars, reject_if: :all_blank, allow_destroy: :true
 
   #fields validation
   validates :ordernum, presence: true, if: ->(order) {order.persisted?}, uniqueness: true, length: { maximum: 128 }#, format: { with: /\A[a-z\d][a-z\d-]*[a-z\d-]\z/i }
   validates :date, presence: true
   validates :customer_id, presence: true
+  validates :auction_id, presence: true
   validates :lotstock, presence: true, numericality: { greater_than_or_equal_to: 0}
   validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 1 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0}
   validates :total, presence: true, numericality: { greater_than_or_equal_to: :price}
-  validates :orderstatus, presence: true
+  validates :orderstatus_id, presence: true
 
   def order_info
     "#{ordernum} (#{customer.first_name} #{customer.last_name})"
