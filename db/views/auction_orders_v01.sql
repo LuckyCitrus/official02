03e5233e836b3a0275a412cc0acc558f2ab8e22d
Kiet's Desktop <kmnguy25@gmail.com>
2020-04-14 17:24:29 -0500

    pre_delete

--- db/views/auction_orders_v01.sql
+++ db/views/auction_orders_v01.sql
@@ -1,6 +1,6 @@
 SELECT auctionname, sum(o.quantity) as total_orders
 FROM auctions a JOIN cars c on c.auction_id = a.id
-JOIN orders o on cars.order_id = o.id
+JOIN orders o on cars.order_id = c.order_id
 WHERE date > CURRENT_DATE - INTERVAL '1 month'
 GROUP BY auctionname
 ORDER BY total_orders DESC;
